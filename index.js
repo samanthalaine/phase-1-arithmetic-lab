e454bf178e27ccdc547ab5d145aa785e6ab0748b
Samantha Laine <samanthalaine8@gmail.com>
2021-07-19 22:27:09 -0400

    commit tree aa8a540f302fede5183a90258cb3247e94249e42

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2)
+let num3 = 44
+let num4 = 10
+let set = (2,4,6,8,20)
+
+let random = Number.parseInt((Math.random() +1),10)
+let mod = (num3 % num4)
+let max = Math.max(set)
